05818a79e2394dd1eec8005da20c66cf7972fb71
Kirtan Sakariya <kirtansakariya@gmail.com>
2018-05-12 19:35:08 -0400

    working on joining voice channel

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var discord = require('discord.io');
+var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 
@@ -8,7 +8,7 @@ logger.add(logger.transports.Console, {
 });
 logger.level = 'debug';
 
-var bot = new discord.Client({
+var bot = new Discord.Client({
   token: auth.token,
   autorun: true
 });
@@ -22,6 +22,7 @@ bot.on('message', function(user, userID, channelID, message, evt) {
     var args = message.substring(1).split(' ');
     var cmd = args[0];
 
+    console.log(typeof(bot));
     args = args.splice(1);
     switch(cmd) {
       case 'ping':
@@ -29,6 +30,10 @@ bot.on('message', function(user, userID, channelID, message, evt) {
           to: channelID,
           message: 'Pong!'
         });
+        console.log("user: " + typeof(user) + "\nuserID: " + typeof(userID) + "\nchannelID: " + typeof(channelID) + "\nmessage: " + typeof(message) + "\nevt: " + typeof(evt));
+        console.log(message.member);
+        console.log("user: " + user + "\nuserID: " + userID + "\nchannelID: " + channelID + "\nmessage: " + message + "\nevt: " + evt);
+        console.log(bot.users);
       break;
     }
   }
