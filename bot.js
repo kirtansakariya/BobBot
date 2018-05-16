e4c4a3692e752b422a9268d7f163f9f3fbab6f63
Kirtan Sakariya <kirtansakariya@gmail.com>
2018-05-16 16:03:11 -0400

    can join voice channel now

--- bot.js
+++ bot.js
@@ -1,41 +1,34 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-  colorize: true
-});
-logger.level = 'debug';
-
-var bot = new Discord.Client({
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+const bot = new Discord.Client({
   token: auth.token,
   autorun: true
 });
 bot.on('ready', function(evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
+  console.log("BobBot is ready");
 });
 bot.on('message', message => {
-  console.log(message.member);
-  if (message.substring(0, 1) == ';') {
-    var args = message.substring(1).split(' ');
+//  console.log(message);
+//  console.log(message.guild);
+  console.log(message.content);
+  if (message.content.substring(0, 1) == ';') {
+    var args = message.content.substring(1).split(' ');
     var cmd = args[0];
-
-    console.log(typeof(bot));
     args = args.splice(1);
     switch(cmd) {
       case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong!'
-        });
-        console.log("user: " + typeof(user) + "\nuserID: " + typeof(userID) + "\nchannelID: " + typeof(channelID) + "\nmessage: " + typeof(message) + "\nevt: " + typeof(evt));
-        console.log(message.member);
-        console.log("user: " + user + "\nuserID: " + userID + "\nchannelID: " + channelID + "\nmessage: " + message + "\nevt: " + evt);
-        console.log(bot.users);
+        console.log(bot.voiceConnections[0] + "\n\n\n\n\n\n\n" + bot.voiceConnections[1]);
+        console.log(message.channel.connection);
+        message.channel.send('pong');
+        message.member.voiceChannel.join()
+          .then(connection => {
+            message.reply("hello");
+            console.log("catching");
+          })
+          .catch(console.log);
       break;
     }
   }
 });
+
+bot.login(auth.token);
