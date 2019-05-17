731dbe5b4257dc9f7e8490c4133877e2e3ec669a
Kirtan Sakariya <kirtansakariya@gmail.com>
2019-05-16 20:21:50 -0500

    working on basic views

--- src/database/db.js
+++ src/database/db.js
@@ -8,11 +8,11 @@ const client = new pg.Client({
 client.connect();
 
 /**
- * Add new users to the database.
- * @param {String} username discord name
- * @param {String} discordId discord account identifier
- * @param {String} status status of the user's account
- * @param {Object} callback callback to leave the function
+ * Add a new user to the database.
+ * @param {String} username Discord name
+ * @param {String} discordId Discord account identifier
+ * @param {String} status Status of the user's account
+ * @param {Object} callback Callback to leave the function
  */
 function addUser(username, discordId, status, callback) {
   const queryConfig = {
@@ -22,12 +22,12 @@ function addUser(username, discordId, status, callback) {
 
   client.query(queryConfig, (error, results) => {
     if (error) {
-      console.log('OH NO');
-      console.log(error);
+      console.log('OH NO in addUser');
+      // console.log(error);
       callback(false);
     } else {
-      console.log('OH YES');
-      console.log(results);
+      console.log('OH YES in addUser');
+      // console.log(results);
       callback(true);
     }
   });
@@ -35,8 +35,8 @@ function addUser(username, discordId, status, callback) {
 
 /**
  * Get a user from the database.
- * @param {String} discordId discord account identifier
- * @param {Object} callback callback to leave the function
+ * @param {String} discordId Discord account identifier
+ * @param {Object} callback Callback to leave the function
  */
 function getUser(discordId, callback) {
   const queryConfig = {
@@ -46,12 +46,61 @@ function getUser(discordId, callback) {
 
   client.query(queryConfig, (error, results) => {
     if (error) {
-      console.log('OH NO');
-      console.log(error);
+      console.log('OH NO in getUser');
+      // console.log(error);
       callback(null);
     } else {
-      console.log('OH YES');
-      console.log(results);
+      console.log('OH YES in getUser');
+      // console.log(results);
+      callback(results);
+    }
+  });
+}
+
+/**
+ * Add a new session to the database.
+ * @param {String} sid Session id
+ * @param {String} discordId Discord account identifier
+ * @param {Object} callback Callback to leave the function
+ */
+function addSession(sid, discordId, callback) {
+  const queryConfig = {
+    text: 'INSERT INTO sessions(sid, discord_id) VALUES($1, $2);',
+    values: [sid, discordId],
+  };
+
+  client.query(queryConfig, (error, results) => {
+    if (error) {
+      console.log('OH NO in addSession');
+      // console.log(error);
+      callback(false);
+    } else {
+      console.log('OH YES in addSession');
+      // console.log(results);
+      callback(true);
+    }
+  });
+}
+
+/**
+ * Get a user from the database.
+ * @param {String} sid Session id
+ * @param {Object} callback Callback to leave the function
+ */
+function getSession(sid, callback) {
+  const queryConfig = {
+    text: 'SELECT * FROM sessions WHERE sid = $1',
+    values: [sid],
+  };
+
+  client.query(queryConfig, (error, results) => {
+    if (error) {
+      console.log('OH NO in getSession');
+      // console.log(error);
+      callback(null);
+    } else {
+      console.log('OH YES in getSession');
+      // console.log(results);
       callback(results);
     }
   });
@@ -60,4 +109,6 @@ function getUser(discordId, callback) {
 module.exports = {
   addUser: addUser,
   getUser: getUser,
+  addSession: addSession,
+  getSession: getSession,
 };
--- src/frontend/js/web.js
+++ src/frontend/js/web.js
@@ -1,10 +1,116 @@
 const express = require('express');
 const db = require('../../database/db');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
-const pg = require('pg');
+// const pg = require('pg');
+// let counter = 0;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.use(cookieSession({
+//   name: 'session',
+//   secret: 'shh',
+//   maxAge: 24 * 60 * 60 * 1000,
+// }));
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'shhshhshshshshsh', // just a long random string
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/../views');
+// console.log(__dirname);
+app.engine('hbs', hbs( {
+  extname: '.hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/../views/layouts/',
+}));
+
+app.get('/', (req, res) => {
+  console.log(req.localID);
+  db.getSession(req.sessionID, (results) => {
+    if (results === null) { // Error encountered
+      console.log('error encounterd'); // maybe add session?
+      res.redirect('/error');
+    } else if (results.rows.length === 0) { // No session data
+      // db.addSession(req.sessionID, null, (boo) => {
+      //   if (boo == false) {
+      //     console.log('rip, error');
+      //   } else {
+      //     console.log('yay, added, redirection to login page');
+      //     res.redirect('/login');
+      //   }
+      // });
+      res.redirect('/login');
+    } else if (results.rows[0].discord_id === null) { // Session data but no one is logged in
+      console.log('redirect to login page');
+      res.redirect('/login');
+    } else { // User is logge din
+      console.log('redirect to home page');
+      res.redirect('/home');
+    }
+  });
+});
+
+app.get('/login', (req, res) => {
+  console.log(req.query);
+  db.getSession(req.sessionID, (results) => {
+    if (results === null) { // Error encountered
+      console.log('error encounterd'); // maybe add session?
+      res.redirect('/error');
+    } else if (results.rows.length === 0) { // No session data
+      // db.addSession(req.sessionID, null, (boo) => {
+      //   if (boo == false) {
+      //     console.log('rip, error');
+      //   } else {
+      //     console.log('yay, added, redirectiont o login page');
+      //     res.redirect('/login');
+      //   }
+      // });
+      res.render('login', {layout: 'default'});
+    } else if (results.rows[0].discord_id === null) { // Session data but no one is logged in
+      console.log('redirect to login page');
+      res.render('login', {layout: 'default'});
+    } else { // User is logged in
+      console.log('redirect to home page');
+      res.redirect('/home');
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  console.log('POST in /login');
+  console.log(req.body);
+});
+
+app.get('/signup', (req, res) => {
+  db.getSession(req.sessionID, (results) => {
+    if (results == null) { // Error encountered
+      console.log('error encountered');
+      res.redirect('/error');
+    } else if (results.rows.length === 0) { // No session data
+      res.render('signup', {layout: 'default'});
+    } else if (results.rows[0].discord_id === null) { // Session data but no one is logged in
+      res.render('signup', {layout: 'default'});
+    } else { // User is logged in
+      res.redirect('/home');
+    }
+  });
+});
+
+app.get('/error', (req, res) => {
+  console.log('error nooo');
+  res.render('error', {layout: 'default', data: 'is this', template: 'error-template', subtitle: 'Error'});
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
@@ -44,9 +150,9 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 //   console.log('Opeartion was successful: ' + boo);
 // });
 
-db.getUser('t_id', function(results) {
-  console.log(results);
-});
+// db.getUser('t_id', function(results) {
+//   console.log(results);
+// });
 
 // client.query('INSERT INTO users (username, discord_id, status) VALUES ($1, $2, $3);', [user, discordId, status], (error, results) => {
 //   if (error) {
