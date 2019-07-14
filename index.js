4e17b72356236709e9f46c1eea479a40cae2629b
Kirtan Sakariya <kirtansakariya@gmail.com>
2019-07-14 19:20:19 -0400

    fixing bot idle issue

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const bot = require('./src/discord/bot').bot;
 const app = require('./src/frontend/js/web').app;
+const https = require('https');
 const port = process.env.PORT || 5000;
 
 bot.login(((process.env.TOKEN !== undefined) ? process.env.TOKEN : require('./auth.json').token));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+setInterval(() => {
+  https.get('https://the-bobbot.herokuapp.com/');
+}, 1500000);
