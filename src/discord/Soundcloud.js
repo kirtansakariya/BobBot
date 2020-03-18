865f0ddac0311771210524e7f68f5ea0c8faed09
Kirtan Sakariya <kirtansakariya@gmail.com>
2020-03-18 17:36:38 -0400

    going back to old api

--- src/discord/Soundcloud.js
+++ src/discord/Soundcloud.js
@@ -58,91 +58,65 @@ function addSoundcloud(arr, u, discordId, username, callback) {
   let thumbnail;
   let channel;
   // console.log('SC');
-  https.get('https://api-v2.soundcloud.com/resolve?url=' + u + '&client_id=' + ((process.env.SCID !== undefined) ? process.env.SCID : require('../../auth.json').scid), function(resp) {
+  http.get('http://api.soundcloud.com/resolve?url=' + u + '&client_id=' + ((process.env.SCID !== undefined) ? process.env.SCID : require('../../auth.json').scid), function(resp) {
     let data1 = '';
     resp.on('data', (chunk) => {
       data1 += chunk;
     });
 
     resp.on('end', () => {
+      // console.log(data1);
       data1 = JSON.parse(data1);
-      track = data1;
-      if (track != null) {
-        if (track.kind == 'track') {
-          duration = track.duration;
-          minutes = Math.floor(duration / 60000);
-          seconds = ((duration % 60000) / 1000).toFixed(0);
-          thumbnail = track.artwork_url;
-          if (thumbnail === null) thumbnail = track.user.avatar_url;
-          if (!thumbnail.includes('default_avatar_large.png')) thumbnail = thumbnail.replace('large', 't300x300');
-          channel = track.user.username;
-          arr.push(new Soundcloud(u, track.stream_url, track.title, minutes + ':' + (seconds < 10 ? '0' : '') + seconds, discordId, username, thumbnail, channel));
-          callback();
-        } else {
-          console.log('adding soundcloud playlist');
-          parsePlaylist(arr, discordId, username, track.tracks, callback);
-        }
-      }
+      https.get(data1.location, (resp) => {
+        let data2 = '';
+        resp.on('data', (chunk) => {
+          data2 += chunk;
+        });
+
+        resp.on('end', (chunk) => {
+          track = JSON.parse(data2);
+          if (track != null) {
+            // console.log(track);
+            if (track.kind == 'track') {
+              // console.log('adding singular soundcloud track');
+              duration = track.duration;
+              minutes = Math.floor(duration / 60000);
+              seconds = ((duration % 60000) / 1000).toFixed(0);
+              thumbnail = track.artwork_url;
+              if (thumbnail === null) thumbnail = track.user.avatar_url;
+              if (!thumbnail.includes('default_avatar_large.png')) thumbnail = thumbnail.replace('large', 't300x300');
+              channel = track.user.username;
+              arr.push(new Soundcloud(u, track.stream_url, track.title, minutes + ':' + (seconds < 10 ? '0' : '') + seconds, discordId, username, thumbnail, channel));
+              callback();
+            } else {
+              // console.log('adding soundcloud playlist');
+              while (track.tracks.length > 0) {
+                const t = track.tracks.shift();
+                duration = t.duration;
+                minutes = Math.floor(duration / 60000);
+                seconds = ((duration % 60000) / 1000).toFixed(0);
+                console.log(t);
+                console.log(t.artwork_url);
+                thumbnail = t.artwork_url;
+                if (thumbnail === null) thumbnail = t.user.avatar_url;
+                console.log(thumbnail);
+                if (!thumbnail.includes('default_avatar_large.png')) thumbnail = thumbnail.replace('large', 't300x300');
+                channel = t.user.username;
+                arr.push(new Soundcloud(t.permalink_url, t.stream_url, t.title, minutes + ':' + (seconds < 10 ? '0' : '') + seconds, discordId, username, thumbnail, channel));
+              }
+              callback();
+            }
+          }
+        });
+      }).on('error', (err) => {
+        console.log('error: ' + err.message);
+      });
     });
   }).on('error', (err) => {
     console.log('error: ' + err.message);
   });
   // console.log('outside');
 }
-
-/**
- * Parses a list of SoundCloud track ids and adds them to the playlist
- * @param {Object} arr Array of songs that were found
- * @param {String} discordId Discord identifier of the player
- * @param {String} username Discord username of the player
- * @param {Object} playlist Songs to be added
- * @param {Object} callback Callback to leave the function
- */
-function parsePlaylist(arr, discordId, username, playlist, callback) {
-  tracks = [];
-  for (let i = 0; i < playlist.length && i < 15; i++) {
-    tracks[i] = playlist.shift().id;
-  }
-  https.get('https://api-v2.soundcloud.com/tracks?ids=' + tracks + '&client_id=' + ((process.env.SCID !== undefined) ? process.env.SCID : require('../../auth.json').scid), (resp) => {
-    let data = '';
-
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    resp.on('uncaughtException', (err) => {
-      console.log(err);
-    });
-
-    resp.on('end', () => {
-      const parsed = JSON.parse(data);
-      for (let i = 0; i < tracks.length; i++) {
-        const id = tracks[i];
-        for (let j = 0; j < parsed.length; j++) {
-          const track = parsed[j];
-          if (track.id === id) {
-            duration = track.duration;
-            minutes = Math.floor(duration / 60000);
-            seconds = ((duration % 60000) / 1000).toFixed(0);
-            thumbnail = track.artwork_url;
-            if (thumbnail === null) {
-              thumbnail = track.user.avatar_url;
-            } else if (!thumbnail.includes('default_avatar_large.png')) {
-              thumbnail = thumbnail.replace('large', 't300x300');
-            }
-            channel = track.user.username;
-            arr.push(new Soundcloud(track.permalink_url, track.stream_url, track.title, minutes + ':' + (seconds < 10 ? '0' : '') + seconds, discordId, username, thumbnail, channel));
-          }
-        }
-      }
-      if (playlist.length === 0) {
-        callback();
-      } else {
-        parsePlaylist(arr, discordId, username, playlist, callback);
-      }
-    });
-  });
-};
 
 /**
  * Search SoundCloud based off the serach query that was specified.
@@ -153,7 +127,7 @@ function parsePlaylist(arr, discordId, username, playlist, callback) {
  */
 function scSearch(str, id, searches, callback) {
   console.log('scSearch');
-  https.get('https://api-v2.soundcloud.com/search?q=' + str + '&client_id=' + ((process.env.SCID !== undefined) ? process.env.SCID : require('../../auth.json').scid), function(resp) {
+  http.get('http://api.soundcloud.com/tracks?q=' + str + '&client_id=' + ((process.env.SCID !== undefined) ? process.env.SCID : require('../../auth.json').scid), function(resp) {
     let data = '';
 
     resp.on('data', (chunk) => {
@@ -163,17 +137,17 @@ function scSearch(str, id, searches, callback) {
     resp.on('end', () => {
       searches[id] = [];
       let parsed = JSON.parse(data);
-      parsed = parsed.collection.slice(0, 5);
+      // console.log(parsed);
+      // console.log(parsed.length);
+      parsed = parsed.slice(0, 5);
       for (let i = 0; i < parsed.length; i++) {
         const duration = parsed[i].duration;
         minutes = Math.floor(duration / 60000);
         seconds = ((duration % 60000) / 1000).toFixed(0);
         thumbnail = parsed[i].artwork_url;
-        if (thumbnail === null) {
-          thumbnail = track.user.avatar_url;
-        } else if (!thumbnail.includes('default_avatar_large.png')) {
-          thumbnail = thumbnail.replace('large', 't300x300');
-        }
+        if (thumbnail === null) thumbnail = parsed[i].user.avatar_url;
+        console.log(thumbnail);
+        if (!thumbnail.includes('default_avatar_large.png')) thumbnail = thumbnail.replace('large', 't300x300');
         channel = parsed[i].user.username;
         searches[id][i] = new Soundcloud(parsed[i].permalink_url, parsed[i].stream_url, parsed[i].title, minutes + ':' + (seconds < 10 ? '0' : '') + seconds, null, null, thumbnail, channel);
       }
